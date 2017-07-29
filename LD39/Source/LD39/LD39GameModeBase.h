7cb296a28af6e43bd935c3a382a6b563ee670a2d
Caleb Smith <calebsmth54@gmail.com>
2017-07-28 18:02:55 -0700

    Initial commit

---
+++ LD39/Source/LD39/LD39GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LD39GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LD39_API ALD39GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
